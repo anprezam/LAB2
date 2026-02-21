07b506516e252d81a0fe93d2be02d0394791cf1d
anprezam <anp.rezam@gmail.com>
2026-02-20 21:38:09 -0500

    Update LAB2.py

--- LAB2.py
+++ LAB2.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
+df = pd.read_csv("tu_archivo.csv") 
 st.title("🏥 ED Patient Data — Overview")
 
 # ── Sidebar filters ──────────────────────────────────────────────
